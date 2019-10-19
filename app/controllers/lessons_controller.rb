903d37c3962cc2941b37ac79671f237a4fd9483c
Jonathan Garton <jon.garton94@gmail.com>
2019-10-19 00:57:09 +0000

    payments with stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,14 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson.section.course, only: [:show]
+  #double check line above
+
   def show
+   if current_lesson.section.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized and redirect_to lessons_path(current_section.course)
+      #double check line above, do not understand Flash message instead of unauthorized
+      # redirect_to lesson_path(current_section.course), alert 'You are not enrolled in this course'
+    end
   end
 
   private
