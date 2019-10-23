965ce39d9ce6a8b2d7b34e9ea677e2c81e290c5c
Jonathan Garton <jon.garton94@gmail.com>
2019-10-23 18:40:12 +0000

    updated lessons controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,17 +1,22 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_lesson, only: [:show]
-  #double check line above
+  before_action require_enrollment_for_current_lesson, only: [:show]
+  
 
   def show
-   if not current_lesson.course.any_enrollment?(current_user)
-      return render plain: 'Unauthorized', status: :unauthorized and redirect_to lessons_path(@section)
+   #if not current_lesson.course.any_enrollment?(current_user)
+      #return render plain: 'Unauthorized', status: :unauthorized and redirect_to lessons_path(lesson)
       #double check line above, do not understand Flash message instead of unauthorized
       # redirect_to lesson_path, alert 'You are not enrolled in this course'
-    end
+    #end
   end
 
   private
+
+    def require_enrollment_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render_plain: "Unauthorized", status: :unauthorized
+    end
 
   helper_method :current_lesson
     def current_lesson
